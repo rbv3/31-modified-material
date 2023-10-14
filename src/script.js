102672f56949b1373b3a070dbeff58c83742325e
Rodrigo Valenca <rvalenca@amazon.com>
2023-10-14 15:46:03 -0300

    animate meshStandardMaterial

--- src/script.js
+++ src/script.js
@@ -69,6 +69,40 @@ const material = new THREE.MeshStandardMaterial( {
     normalMap: normalTexture
 })
 
+const customUniforms = {
+    uTime: { value: 0 }
+}
+
+material.onBeforeCompile = (shader) => {
+    shader.uniforms.uTime = customUniforms.uTime
+    shader.vertexShader = shader.vertexShader.replace(
+        "#include <common>",
+        `
+            #include <common>
+
+            uniform float uTime;
+
+            mat2 get2dRotateMatrix(float _angle) {
+                return mat2(
+                    cos(_angle), -sin(_angle),
+                    sin(_angle), cos(_angle)
+                );
+            }
+        `,
+    );
+    shader.vertexShader = shader.vertexShader.replace(
+        '#include <begin_vertex>',
+        `
+            #include <begin_vertex>
+
+            float angle = (position.y + uTime)* 0.9;
+            mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+            transformed.xz = transformed.xz * rotateMatrix;
+        `
+    )
+}
+
 /**
  * Models
  */
@@ -156,6 +190,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update material
+    customUniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
